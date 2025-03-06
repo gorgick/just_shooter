6b6e3da860b98214749d6ca96465175a2d9da982
gorgick <gorgick2014@yandex.by>
2025-03-06 18:34:59 +0300

    requirements, images

---
+++ main.py
@@ -0,0 +1,14 @@
+import os
+
+import pygame
+
+SPACE_SHIP_1 = pygame.image.load(os.path.join("images", "1_monstr.png"))
+SPACE_SHIP_2 = pygame.image.load(os.path.join("images", "2_monstr.png"))
+SPACE_SHIP_3 = pygame.image.load(os.path.join("images", "3_monstr.png"))
+
+MAIN_SPACE_SHIP = pygame.image.load(os.path.join("images", "airplane.jpg"))
+
+LASER = pygame.image.load(os.path.join("assets", "pixel_laser_green.png"))
+
+
+BG = pygame.image.load(os.path.join("images", "backgr.jpg"))
